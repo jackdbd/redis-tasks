f10352576ccbd892c2ed8b47112f8074c616c762
jackdbd <jackdebidda@gmail.com>
2018-08-02 09:43:53 +0200

    Add Express server configuration

--- app.js
+++ app.js
@@ -8,3 +8,18 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function(req, res) {
+  res.send("Hi!");
+});
+
+const PORT = 3000;
+app.listen(PORT);
+console.log(`Express server started on port ${PORT}`);
+
+module.exports = app;
